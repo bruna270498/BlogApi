48f07a917ad617eeb56bd4da1e58f6d81b730f41
Bruna <brunaalmeida075@gmail.com>
2023-04-10 15:45:51 -0300

    requisito 2 ok

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+const User = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+
+    }, {
+        timestamps: false,
+        underscored: true,
+        tableName: 'users'
+    }, )
+    User.associate = (model) => {
+        User.hasMany(model.blog_posts, {
+            foreignKey: 'user_id', as: 'blog_posts'
+        })
+    }
+    return User;
+};
+module.exports = User;
